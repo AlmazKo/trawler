9321c22dbfcb57e6ceb1ad73b7e90ed669105ac1
Alex Suslov <a.s.suslov@gmail.com>
2017-10-20 00:45:00 +0300

    upd block counter

--- src/background.ts
+++ src/background.ts
@@ -5,6 +5,13 @@ import { ad_rules } from "./ad_rules";
 import { iframe_white_rules } from "./iframe_white_rules";
 
 const tabs = chrome.tabs;
+let counter = 0;
+
+chrome.browserAction.setBadgeBackgroundColor({color: "#74A2B0"});
+
+function setBadge(val: any) {
+  chrome.browserAction.setBadgeText({text: val + ''});
+}
 
 function iFrameIsAvailable(req: WebRequestDetails) {
   return iframe_white_rules.some(u => u.test(req.url.toLowerCase()));
@@ -29,6 +36,8 @@ function logBlocked(req: WebRequestHeadersDetails, tab?: Tab) {
 chrome.webRequest.onBeforeRequest.addListener(
   req => {
     const block = isBlock(req);
+    if (block) setBadge(++counter);
+
     if (block && req.tabId >= 0) {
       tabs.get(req.tabId, tab => logBlocked(req, tab))
     } else if (block) {
