f9a64a78fe928604b8fdd88e50ebd35c41687dc3
Alexander Suslov <Alexander.Suslov@alpari.org>
2017-10-19 10:30:56 +0300

    remove blocking youtube iframes

--- src/background.ts
+++ src/background.ts
@@ -1,31 +1,39 @@
 import WebRequestDetails = chrome.webRequest.WebRequestDetails;
-import {db} from "./db";
 import Tab = chrome.tabs.Tab;
 import WebRequestHeadersDetails = chrome.webRequest.WebRequestHeadersDetails;
+import { db } from "./db";
 
 const tabs = chrome.tabs;
 
+function iFrameIsAvailable(req: WebRequestDetails) {
+  return /^https:\/\/www.youtube.com/.test(req.url);
+}
+
 function isBlock(req: WebRequestDetails) {
-    return req.frameId > 0 || db.some(u => req.url.indexOf("://" + u) !== -1)
+  if (req.frameId > 0 && iFrameIsAvailable(req)) {
+    return false;
+  }
+
+  return db.some(u => req.url.indexOf("://" + u) !== -1)
 }
 
 function logBlocked(req: WebRequestHeadersDetails, tab?: Tab) {
-    if (tab) {
-        console.log('%c  ', `background:url(${tab.favIconUrl}) no-repeat; background-size: 13px 13px;`, 'block', req.url);
-    } else {
-        console.log('? ', 'block', req)
-    }
+  if (tab) {
+    console.log('%c  ', `background:url(${tab.favIconUrl}) no-repeat; background-size: 13px 13px;`, 'block', req.url);
+  } else {
+    console.log('? ', 'block', req)
+  }
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
-    req => {
-        const block = isBlock(req);
-        if (block && req.tabId >= 0) {
-            tabs.get(req.tabId, tab => logBlocked(req, tab))
-        } else if (block) {
-            logBlocked(req);
-        }
-        return {cancel: block}
-    },
-    {urls: ["<all_urls>"]},
-    ["blocking"]);
+  req => {
+    const block = isBlock(req);
+    if (block && req.tabId >= 0) {
+      tabs.get(req.tabId, tab => logBlocked(req, tab))
+    } else if (block) {
+      logBlocked(req);
+    }
+    return {cancel: block}
+  },
+  {urls: ["<all_urls>"]},
+  ["blocking"]);
