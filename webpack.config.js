6f2628488f628e161cca201b545c94b3c074cb31
Alexander Suslov <Alexander.Suslov@alpari.org>
2017-10-19 16:12:49 +0300

    #3 add script for full build

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
-const path = require('path');
+const path            = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: {
-        inject: './src/inject.ts',
+    entry  : {
+        inject    : './src/inject.ts',
         background: './src/background.ts'
     },
-    output: {
+    output : {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
+        path    : path.resolve(__dirname, 'dist')
     },
     resolve: {
         extensions: [".ts"]
     },
-    module: {
+    module : {
         loaders: [
-            {  loader: "ts-loader" }
+            {loader: "ts-loader"}
         ]
-    }
+    },
+
+    plugins: [
+        //plugin doc: https://www.npmjs.com/package/copy-webpack-plugin
+        new CopyWebpackPlugin([{from: 'static'}])
+    ]
 };
